45d8eaa4dd4ac121e2a3fbb1da5d23306730f737
Wu Xiaohua <xhwu@wus-imac.local>
2018-08-16 12:04:21 -0500

    Initial Commit

---
+++ NetworkInfo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkInfo
+//
+//  Created by Wu Xiaohua on 8/16/18.
+//  Copyright © 2018 Wu Xiaohua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
