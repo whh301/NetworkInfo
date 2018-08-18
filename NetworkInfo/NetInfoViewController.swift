419eda7d7bdacb5d3c405e0913f8afa3af1019b8
Xiaohua Wu <whh301@gmail.com>
2018-08-17 22:36:55 -0500

    for menu bar

--- NetworkInfo/NetInfoViewController.swift
+++ NetworkInfo/NetInfoViewController.swift
@@ -1,52 +1,63 @@
 //
 //  NetInfoViewController.swift
-//  UIPageViewController Post
+//  NetworkInfo
 //
-//  Created by Jeffrey Burt on 2/3/16.
-//  Copyright © 2016 Seven Even. All rights reserved.
+//  Created by xhwu on 8/17/18.
+//  Copyright © 2018 Wu Xiaohua. All rights reserved.
 //
 
 import UIKit
 
 class NetInfoViewController: UIViewController {
 
-    @IBOutlet weak var pageControl: UIPageControl!
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var leadingC: NSLayoutConstraint!
+    @IBOutlet weak var trailingC: NSLayoutConstraint!
     
-    var netInfoPageViewController: NetInfoPageViewController? {
-        didSet {
-            netInfoPageViewController?.netInfoDelegate = self
+    @IBOutlet weak var ubeView: UIView!
+    
+    var isMenuIsVisible = false
+    
+    @IBAction func btnTapped(_ sender: Any) {
+        
+        if !isMenuIsVisible {
+            leadingC.constant = 150
+            trailingC.constant = -150
+            
+            isMenuIsVisible = true
+        } else {
+            leadingC.constant = 0
+            trailingC.constant = 0
+            
+            isMenuIsVisible = false
         }
+        
+        // Animation
+        
     }
     
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        pageControl.addTarget(self, action: Selector(("didChangePageControlValue")), for: .valueChanged)
+
+        // Do any additional setup after loading the view.
     }
 
-    @IBAction func didTapNextButton(sender: UIButton) {
-        netInfoPageViewController?.scrollToNextViewController()
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    /**
-     Fired when the user taps on the pageControl to change its current page.
-     */
-    func didChangePageControlValue() {
-        netInfoPageViewController?.scrollToViewController(index: pageControl.currentPage)
-    }
-}
 
-extension NetInfoViewController: NetInfoPageViewControllerDelegate {
-    
-    func netInfoPageViewController(netInfoPageViewController: NetInfoPageViewController,
-        didUpdatePageCount count: Int) {
-        pageControl.numberOfPages = count
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-    func netInfoPageViewController(netInfoPageViewController: NetInfoPageViewController,
-        didUpdatePageIndex index: Int) {
-        pageControl.currentPage = index
-    }
-    
+    */
+
 }
