4e5b596a2b6301a2602508db02dc00be1a75f836
Xiaohua Wu <whh301@gmail.com>
2018-08-19 09:43:29 -0500

    menu done
    menu done

--- NetworkInfo/NetInfoViewController.swift
+++ NetworkInfo/NetInfoViewController.swift
@@ -32,12 +32,13 @@ class NetInfoViewController: UIViewController {
         }
         
         // Animation
-        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { (animationComplete) in
+            print("The animation is complete!")
+        }
     }
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
