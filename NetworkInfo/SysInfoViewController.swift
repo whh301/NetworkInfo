e14c933adfa99ecb20053f722ed74edef6db1235
Xiaohua Wu <whh301@gmail.com>
2018-08-20 09:04:30 -0500

    sys info tab

---
+++ NetworkInfo/SysInfoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SysInfoViewController.swift
+//  NetworkInfo
+//
+//  Created by Wu Xiaohua on 8/20/18.
+//  Copyright © 2018 Wu Xiaohua. All rights reserved.
+//
+
+import UIKit
+
+class SysInfoViewController: UIViewController {
+
+    // MARK: Label variants
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupSysInfo()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Set the system information
+    private func setupSysInfo() {
+        
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
